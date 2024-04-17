c082ec2c8c7bf944c5142dcd48429decefa6847d
Faraz <farazrze18@gmail.com>
2024-04-17 11:47:57 +0330

    static all html's files

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',index)
+    path('',index),
+    path('contact',contact),
+    path('shop',shop),
+    path('product',product)
 ]
--- website/views.py
+++ website/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def product(request):
+    return render(request,'product-details.html')
+
+def shop(request):
+    return render(request,'shop.html')
